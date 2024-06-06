11acc66d4c7004548287821e94877fa3fafe6c4e
PriyanshuPandey10 <pandey.priyanshu10@gmail.com>
2024-06-06 12:44:33 +0530

    routing

--- app/page.js
+++ app/page.js
@@ -1,15 +1,16 @@
-"use client"
-import React, { useState } from 'react'
-import Header from '@/Components/Header'
-const page = () => {
-    const [user, setuser] = useState("Priyanshu")
-  return (
-    <>
-      < Header user={user} />
-      {user}
-    
-    </>
-  )
-}
+'use client';
+import React from 'react';
+import Link from 'next/link'; // Correct import statement
 
-export default page
+const Page = () => {
+  return (
+    <div>
+      <h1>This is Home Page</h1>
+      <Link href="/About">About</Link>
+      <Link href="/Product">Product</Link>
+      <Link href="/Courses">Courses</Link>
+    </div>
+  );
+};
+
+export default Page;
